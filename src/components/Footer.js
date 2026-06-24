4c99a55d07c7ff784e64f01254c9ea122f032ede
ahmedpaulo77 <p638599@gmail.com>
2026-06-24 09:58:26 +0300

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
         </div>
 
         <div className="footer-social">
-          <a href="https://facebook.com" target="_blank" rel="noreferrer" className="social-link facebook">
+          <a href="https://web.facebook.com/p/%D8%B6%D9%85%D8%A7%D9%86-Banha-61582103792911/?_rdc=1&_rdr#" target="_blank" rel="noreferrer" className="social-link facebook">
             <i className="ti ti-brand-facebook"></i>
           </a>
           <a href="https://instagram.com" target="_blank" rel="noreferrer" className="social-link instagram">
